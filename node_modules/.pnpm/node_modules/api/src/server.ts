47bffd0c49c2a544fca1d995e42c2b277a5b5dd9
Riyan <riyancomp60@gmail.com>
2026-06-04 18:12:30 +0530

    feat: setup backend foundation and prisma

--- node_modules/.pnpm/node_modules/api/src/server.ts
+++ node_modules/.pnpm/node_modules/api/src/server.ts
@@ -1,4 +1,7 @@
+import dotenv from "dotenv";
 import express from "express";
+
+dotenv.config();
 
 const app = express();
 
@@ -8,7 +11,7 @@ app.get("/", (req, res) => {
   res.send("API Running");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
